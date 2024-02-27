b58cda06dee8230e8e9413e0c2d1345e2f5a2d90
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2024-02-27 17:29:53 +0100

    Remove "break;" / Make program async

--- CompileAllHammerMaps/Program.cs
+++ CompileAllHammerMaps/Program.cs
@@ -1,62 +1,66 @@
 ﻿using System.Diagnostics;
 
-namespace CompileAllHammerMaps;
+Stopwatch sw = new();
+sw.Start();
 
-internal class Program
+// JACK 1.1.1064 related settings
+string pathCFG = "VDKGameCfg.ini";
+string mapSourceFileType = "map";
+
+string pathJACKDir = args[0];
+string pathMapsSourceDir = args[1];
+string pathMapsOutputDir = args[2];
+string argsCSG = args[3];
+string argsBSP = args[4];
+string argsVIS = args[5];
+string argsRAD = args[6];
+
+string[] cfgLines = File.ReadAllLines($"{args[0]}\\{pathCFG}");
+
+string pathCSG = "";
+string pathBSP = "";
+string pathVIS = "";
+string pathRAD = "";
+foreach (string line in cfgLines)
 {
-    static void Main(string[] args)
+    string substring = line[..8];
+    if (substring == "MapTool0")
     {
-        // JACK 1.1.1064 related settings
-        string pathCFG = "VDKGameCfg.ini";
-        string mapSourceFileType = "map";
-
-        string pathJACKDir = args[0];
-        string pathMapsSourceDir = args[1];
-        string pathMapsOutputDir = args[2];
-        string argsCSG = args[3];
-        string argsBSP = args[4];
-        string argsVIS = args[5];
-        string argsRAD = args[6];
-
-        string[] cfgLines = File.ReadAllLines($"{args[0]}\\{pathCFG}");
-
-        string pathCSG = "";
-        string pathBSP = "";
-        string pathVIS = "";
-        string pathRAD = "";
-        foreach (string line in cfgLines) 
-        {
-            string substring = line[..8];
-            if (substring == "MapTool0")
-            {
-                pathCSG = line[9..];
-            }
-            else if (substring == "MapTool1")
-            {
-                pathBSP = line[9..];
-            }
-            else if (substring == "MapTool2")
-            {
-                pathVIS = line[9..];
-            }
-            else if (substring == "MapTool3")
-            {
-                pathRAD = line[9..];
-            }
-        }
-
-        IEnumerable<string> pathMapSources = Directory.GetFiles(pathMapsSourceDir)
-            .Where(line => line.EndsWith($".{mapSourceFileType}"));
-        foreach (string pathMapSource in pathMapSources)
-        {
-            string fileNameWithExtension = pathMapSource[(pathMapsSourceDir.Length + 1)..];
-            string pathMapOutput = $"{pathMapsOutputDir}\\{fileNameWithExtension}";
-
-            File.Copy(pathMapSource, pathMapOutput, true);
-            Process.Start(pathCSG, $"{pathMapOutput} {argsCSG}").WaitForExit();
-            Process.Start(pathBSP, $"{pathMapOutput} {argsBSP}").WaitForExit();
-            Process.Start(pathVIS, $"{pathMapOutput} {argsVIS}").WaitForExit();
-            Process.Start(pathRAD, $"{pathMapOutput} {argsRAD}").WaitForExit();break;
-        }
+        pathCSG = line[9..];
+    }
+    else if (substring == "MapTool1")
+    {
+        pathBSP = line[9..];
+    }
+    else if (substring == "MapTool2")
+    {
+        pathVIS = line[9..];
+    }
+    else if (substring == "MapTool3")
+    {
+        pathRAD = line[9..];
     }
 }
+
+List<Task> compileTasks = [];
+
+IEnumerable<string> pathMapSources = Directory.GetFiles(pathMapsSourceDir)
+    .Where(line => line.EndsWith($".{mapSourceFileType}"));
+foreach (string pathMapSource in pathMapSources)
+{
+    string fileNameWithExtension = pathMapSource[(pathMapsSourceDir.Length + 1)..];
+    string pathMapOutput = $"{pathMapsOutputDir}\\{fileNameWithExtension}";
+
+    compileTasks.Add(Task.Run(() =>
+    {
+        File.Copy(pathMapSource, pathMapOutput, true);
+        Process.Start(pathCSG, $"{pathMapOutput} {argsCSG}").WaitForExit();
+        Process.Start(pathBSP, $"{pathMapOutput} {argsBSP}").WaitForExit();
+        Process.Start(pathVIS, $"{pathMapOutput} {argsVIS}").WaitForExit();
+        Process.Start(pathRAD, $"{pathMapOutput} {argsRAD}").WaitForExit();
+    }));
+}
+
+Task.WaitAll([.. compileTasks]);
+
+Console.WriteLine($"\nTime elapsed: {sw.Elapsed}");
